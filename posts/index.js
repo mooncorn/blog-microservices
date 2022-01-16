7cda39da9f26df5142246522e24d65fe71bd05b7
dankloop <hidasior@gmail.com>
2022-01-16 16:45:12 -0500

    posts and comments

---
+++ posts/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  posts[id] = { id, ...req.body };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Server up on port 4000.');
+});
