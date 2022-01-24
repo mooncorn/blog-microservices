88d8f74ec6314fc18a2c44fbca38cc157d2e4fc9
mooncorn <hidasior@gmail.com>
2022-01-24 11:52:04 -0500

    removed unused files

--- query/index.js
+++ query/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,20 +10,20 @@ app.use(cors());
 const posts = {};
 
 const handleEvent = (type, data) => {
-  if (type === 'PostCreated') {
+  if (type === "PostCreated") {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === 'CommentCreated') {
+  if (type === "CommentCreated") {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
 
-  if (type === 'CommentUpdated') {
+  if (type === "CommentUpdated") {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
@@ -36,11 +36,11 @@ const handleEvent = (type, data) => {
   }
 };
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
   handleEvent(type, data);
@@ -49,16 +49,16 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4002, async () => {
-  console.log('Listening on 4002');
-
+  console.log("Listening on 4002");
   try {
-    const res = await axios.get('http://localhost:4005/events');
+    const res = await axios.get("http://localhost:4005/events");
 
     for (let event of res.data) {
-      console.log('Processing event: ', event.type);
+      console.log("Processing event:", event.type);
+
       handleEvent(event.type, event.data);
     }
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.log(error.message);
   }
 });
